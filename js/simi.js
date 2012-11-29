aa9a52ec52ade4100f847da4328ef090e2447f8b
Valentino Aluigi <valentino.aluigi@gmail.com>
2012-11-29 12:39:39 +0000

    Adding utilities to work with images and bits

--- js/simi.js
+++ js/simi.js
@@ -3,21 +3,77 @@
     var SIMILARITY_THRESHOLD = 0.5;
 
     var utils = {
-        desaturate: function(imageData){
-            for(var i = 0; i < imageData.data.length; i+=4){
-                var average = (imageData.data[i] + imageData.data[i+1] + imageData.data[i+2]) / 3;
-                imageData.data[i] = imageData.data[i+1] = imageData.data[i+2] = average;
+        /**
+         * Returns an array with averaged colors (shades of gray)
+         * @param data
+         */
+        desaturate: function(data){
+            var grays = new Array(data.length / 4);
+            for(var i = 0; i < data.length; i+=4){
+                grays[i] = (data[i] + data[i+1] + data[i+2]) / 3;
             }
+        },
+
+        /**
+         * Returns the average of an array of numbers
+         * @param data
+         * @return Number
+         */
+        average: function(data){
+            var total = 0;
+            for(var i = 0; i < data.length; i++){
+                total += data[i];
+            }
+            return Math.round(total / data.length);
+        },
+
+        /**
+         * Generates a long bitmask (64bit) from an array of 64 bytes.
+         * Each byte is converted in a positive bit if the byte is greater
+         * or equal than the specified threshold
+         *
+         * @param data
+         * @param threshold
+         * @return Array [int lowBits, int hightBits]
+         */
+        thresholdMask: function(data, threshold){
+            return [0,1].map(function(element, index){
+                return utils.mapToBits(data.splice(0,32), function(byte){
+                    return byte >= threshold;
+                });
+            });
+        },
+
+        /**
+         * Generates a bit mask by invoking the callback on each element
+         * of the provided array and computing the result as a series of bit.
+         * The callback must return a boolean.
+         *
+         * @param data
+         * @param callback
+         * @return Number bitMask
+         */
+        mapToBits: function(data, callback){
+            var result = 0, bit = 0;
+            data.forEach(function(element){
+                result |= callback(element) << bit++;
+            });
+            return result;
         }
     };
 
     function hash(image){
         var canvas = document.createElement("canvas");
         canvas.width = 8; canvas.height = 8;
-        canvas.getContext("2d").drawImage(image, 0, 0, 8, 8);
 
-        var imageData = canvas.getImageData();
-        _desaturate(imageData);
+        var context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0, 8, 8);
+
+
+        var imageData = context.getImageData(0, 0, 8, 8);
+        var matrix = utils.desaturate(imageData.data);
+        var average = utils.average(matrix);
+        var bits = utils.bits(matrix, average);
 
         return canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, "");
     }
--- spec/simi.js
+++ spec/simi.js
@@ -1,23 +1,49 @@
-describe("simi", function() {
-  describe("utils", function() {
-    describe("desaturate", function() {
-      it("remove colors from an ImageData", function() {
-          var image = document.getElementById('colorful-picture');
-          var width = image.width,
-              height = image.height;
-          var canvas = document.createElement('canvas');
-          canvas.width = width; canvas.height = height;
+describe("simi", function () {
 
-          var context = canvas.getContext('2d');
-          context.drawImage(image,0,0,width,height);
+    describe("utils", function () {
 
-          var imageData = context.getImageData(0,0,width,height);
+        describe("mapToBits", function () {
+            it("generates a bit mask out of a callback run on each element of an array", function () {
+                var bitMask = simi.utils.mapToBits([1, 2, 3, 4], function (item) {
+                    return item % 2 == 0;
+                });
 
-          simi.utils.desaturate(imageData);
+                assert.equal('1010', bitMask.toString(2));
+            });
+        });
 
-          context.putImageData(imageData,0,0);
-          document.getElementsByTagName('body')[0].appendChild(canvas);
-      });
+        describe("thresholdMask", function () {
+            it("returns the correct lower bits of the mask", function () {
+                var input = new Array(64);
+                input[0] = 10;
+                input[1] = 20;
+                input[2] = 30;
+
+                var threshold = 20;
+
+                var expectedOutput = '110';
+
+                var bitMask = simi.utils.thresholdMask(input, threshold);
+
+                assert.equal(expectedOutput, bitMask[0].toString(2));
+            });
+
+            it("returns the correct higher bits of the mask", function () {
+                var input = new Array(64);
+                input[32+0] = 10;
+                input[32+1] = 20;
+                input[32+2] = 30;
+
+                var threshold = 20;
+
+                var expectedOutput = '110';
+
+                var bitMask = simi.utils.thresholdMask(input, threshold);
+
+                assert.equal(expectedOutput, bitMask[1].toString(2));
+            });
+        });
+
     });
-  });
+
 });
